4276c7269416e412473d7719533533f378bd4aa4
Web Monkey <104577087+zhuk1707@users.noreply.github.com>
2022-08-23 11:25:53 +0300

    rewrite to do logic

--- scripts/to-do-list.js
+++ scripts/to-do-list.js
@@ -1,68 +1,87 @@
-// Create buttons and append it to each list item
-
-function createAndAppendButtons(arr, index = 0) {
-  const buttons = document.createElement('div')
-  buttons.className = 'to-do__buttons'
-
-  const btnComplete = document.createElement('button')
-  btnComplete.className = 'to-do__button_complete'
-  const btnContentCheck = document.createElement('i')
-  btnContentCheck.className = 'fa-solid fa-check'
-  btnComplete.appendChild(btnContentCheck)
-  buttons.appendChild(btnComplete)
-
-  const btnDelete = document.createElement('button')
-  btnDelete.className = 'to-do__button_delete'
-  const btnContentDelete = document.createElement('i')
-  btnContentDelete.className = 'fa-regular fa-trash-can'
-  btnDelete.appendChild(btnContentDelete)
-  buttons.appendChild(btnDelete)
-
-  arr[index].appendChild(buttons)
-}
-
-const nodeList = Array.from(document.querySelectorAll('.to-do__item')) 
-for (let i = 0; i <nodeList.length; i++) {
-  createAndAppendButtons(nodeList, i)
-}
-
-//Add button handlers
-const list = document.querySelector('.to-do__list')
-list.addEventListener('click', function (ev) {
-  if (ev.target.closest('.to-do__button_complete')) {
-    ev.target.parentElement.parentElement.parentElement.classList.toggle('completed')
-  }
-  if (ev.target.closest('.to-do__button_delete')) {
-    console.log(false)
-    const div = ev.target.parentElement.parentElement.parentElement
-    div.remove()
-  }
-}, false)
-
 const form = document.querySelector('.to-do__form')
+const taskInput = document.querySelector('.to-do__input')
+const taskList = document.querySelector('.to-do__list')
 
-form.addEventListener('submit', (e) => {
+let tasks = []
+
+form.addEventListener('submit', addTask)
+
+taskList.addEventListener('click', deleteTask)
+
+taskList.addEventListener('click', markTaskAsCompleted)
+
+// taskList.addEventListener('click', editTask)
+
+
+//functions
+function addTask(e) {
   e.preventDefault()
 
-  const listItem = document.createElement('div')
-  listItem.className = 'to-do__item'
+  const taskText = taskInput.value
+  
+  const newTask = {
+    id: Date.now(),
+    text: taskText,
+    isCompleted: false
+  }
+  
+  tasks.push(newTask)
 
-  const listText = document.createElement('div')
-  listText.className = 'to-do__text'
+  const cssClass = newTask.isCompleted? "to-do__item completed" : 'to-do__item'
+  const taskHTML = `<li class="${cssClass}" id="${newTask.id}">
+                        <input class="to-do__text"
+                               value='${newTask.text}'
+                               readonly required>
+                        <div class="to-do__buttons">
+                            <button type="button"
+                                    class="to-do__button_complete">
+                                <i class="fa-solid fa-check"></i>
+                            </button>
+                            <button type="button" class="to-do__button_edit">
+                                <i class="fa-regular fa-pen-to-square"></i>
+                            </button>
+                            <button type="button" class="to-do__button_delete">
+                                <i class="fa-regular fa-trash-can"></i>
+                            </button>
+                        </div>
+                    </li>`
 
-  const input = document.querySelector('.to-do__input').value
-
-  const text = document.createTextNode(input)
-
-  listItem.appendChild(listText)
-  listText.appendChild(text)
-
-  createAndAppendButtons([listItem])
-  list.appendChild(listItem)
-
-  const taskInput = document.querySelector('.to-do__input')
+  taskList.insertAdjacentHTML('beforeend', taskHTML)
 
   taskInput.value = ''
   taskInput.focus()
-})
+}
 
+function deleteTask(e) {
+  if (e.target.closest('.to-do__button_delete')) {
+    const parentNode = e.target.closest('.to-do__item')
+    const parentId = Number(parentNode.id)
+    const index = tasks.findIndex(task => task.id === parentId)
+
+    tasks.splice(index, 1)
+
+    parentNode.remove()
+  }
+}
+
+function markTaskAsCompleted(e) {
+  if (e.target.closest('.to-do__button_complete')) {
+    const parentNode = e.target.closest('.to-do__item')
+    const parentId = Number(parentNode.id)
+    const task = tasks.find(task => task.id === parentId)
+
+    task.isCompleted = !task.isCompleted
+    
+    parentNode.classList.toggle('completed')
+  }
+}
+
+// function editTask(e) {
+//   if (e.target.closest('.to-do__button_edit')) {
+//     const parentNode = e.target.closest('.to-do__item')
+//     const input = parentNode.querySelector('input')
+//
+//     input.toggleAttribute( 'readonly')
+//     input.focus()
+//   }
+// }
